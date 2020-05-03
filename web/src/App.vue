e544ba7990747adbc93941ad11e81d42667556e1
Half-Shot <will@half-shot.uk>
2020-05-03 15:38:00 +0100

    UI lobby changes

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <main class="root-container">
-    <Game v-if="gameName !== null"/>
-    <Introduction @start-game="onStartGame" v-else/>
+    <Introduction :gameClient="gameClient" @start-game="onStartGame" v-if="clientState === 'home'"/>
+    <Lobby :gameClient="gameClient" :gameName="gameName" v-if="clientState === 'lobby'"/>
+    <Game :gameClient="gameClient" v-if="clientState === 'ingame'"/>
   </main>
 </template>
 
@@ -11,8 +12,10 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import Game from './components/Game.vue';
-import Introduction from './components/Introduction.vue';
+import Game from './components/views/Game.vue';
+import Introduction from './components/views/Introduction.vue';
+import Lobby from './components/views/Lobby.vue';
+import { GameClient, DummyGameClient } from "./GameClient";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
@@ -20,14 +23,19 @@ Vue.use(IconsPlugin);
 @Component({
   components: {
     Introduction,
+    Lobby,
     Game,
   },
 })
 export default class App extends Vue {
-  public gameName: string|null = null;
+  private clientState: "home"|"lobby"|"ingame" = "home";
+  private gameName: string|null = null;
+  private gameClient = new DummyGameClient();
 
   private onStartGame(gameName: string) {
+    gameName = typeof(gameName) === "string" ? gameName : "demo";
     console.log("Game Started", gameName);
+    this.clientState = "lobby";
     this.gameName = gameName;
   }
 }
--- web/src/GameClient.ts
+++ web/src/GameClient.ts
@@ -1,3 +1,73 @@
-export class GameClient {
-    
+export interface PlayerInfo {
+    name: string;
+    score: number;
+    isMaster: boolean;
+    isDicounted: boolean;
+    isReady: boolean;
+}
+
+export interface GameInfo {
+    name: string;
+    players: PlayerInfo[];
+}
+
+export interface GameClient {
+    joinGame: (gameName: string, nickname: string) => Promise<null|string>;
+    getGame: (gameName: string) => Promise<GameInfo>;
+}
+
+export class DummyGameClient implements GameClient {
+    public async joinGame(gameName: string, nickname: string) {
+        // Introduce some delay.
+        await new Promise((resolve) => setTimeout(resolve, 500 * Math.random()));
+        if (gameName === 'demo') {
+            return null;
+        }
+        return "Game not found";
+    }
+
+    public async getGame(gameName: string) {
+        // Introduce some delay.
+        await new Promise((resolve) => setTimeout(resolve, 500 * Math.random()));
+        if (gameName === 'demo') {
+            return {
+                name: "demo",
+                players: [
+                    {
+                        name: "Alice",
+                        score: 0,
+                        isMaster: false,
+                        isDicounted: false,
+                    },
+                    {
+                        name: "Bob",
+                        score: 1,
+                        isMaster: false,
+                        isDicounted: false,
+                    },
+                    {
+                        name: "foo",
+                        score: 0,
+                        isMaster: true,
+                        isDicounted: false,
+                    }
+                ]
+            }
+        }
+        throw Error("Game not found");
+    }
+
+    public async subscribeToStream() {
+
+    }
+}
+
+export class NetGameClient implements GameClient {
+    public async joinGame(gameName: string, nickname: string): Promise<null|string> {
+        throw Error('Not implemented');
+    }
+
+    public async getGame(gameName: string): Promise<GameInfo> {
+        throw Error('Not implemented');
+    }
 }
